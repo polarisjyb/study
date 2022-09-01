2c5ebe15014af5bd9ec2d66077b63137cc5357b0
polarisjyb <evia_a@naver.com>
2022-09-01 15:59:17 +0900

    study

--- javascript/2022.09/2022.09.01 ~ 09.03/2022.09.01/main.js
+++ javascript/2022.09/2022.09.01 ~ 09.03/2022.09.01/main.js
@@ -1,1 +1,19 @@
-console.log(3)
+
+
+
+
+
+function style(style, styleValue ) {
+  if(typeof style === 'string'
+    && typeof styleValue === 'string'
+  ) {
+
+    this.style = style;
+    this.styleValue = styleValue;
+  }
+}
+
+const styleList = new style('width', '100px');
+
+console.log(styleList)
+
