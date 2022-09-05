689908255be35580118b4b38a1980780c3e71a07
polarisjyb <polaris_lsj@naver.com>
2022-09-05 23:00:24 +0900

    구구단

--- javascript/2022.09/2022.09.04 ~ 09.10/2022.09.05/구구단/main.js
+++ javascript/2022.09/2022.09.04 ~ 09.10/2022.09.05/구구단/main.js
@@ -15,8 +15,8 @@ const gogodanArr = [];
 function gogodan(n) {
   //초기값 설정, 비교, 증감
   for(let i = 1; i <= 9; i++){
-    console.log( n + 'x' + i + '=' +  n * i );
-    gogodanArr[i-1] = n + 'x' + i + '=' +  n * i ;
+    console.log( n + ' ' + 'x' + ' ' + i + ' ' +'=' + ' ' +  n * i );
+    gogodanArr[i-1] = n + ' ' + 'x' + ' ' + i + ' ' +'=' + ' ' +  n * i ;
   }
   return gogodanArr
 }
@@ -24,7 +24,7 @@ function gogodan(n) {
 // 구구단 2단을 출력해준다.
 gogodan(2)
 
-console.log(gogodanArr)
+console.log(gogodanArr.length)
 
 // html 문서 script 에서 작성해야 보임
 // function test(input){
@@ -35,8 +35,16 @@ console.log(gogodanArr)
 // }
 // test(2);
 
-const body = document.body
 
+// const html = document.documentElement
+// console.dir(html)
+
+const body = document.body
+// console.dir(body)
+
+body.style.margin = '0';
+body.style.padding = '0';
+body.style.boxSizing = 'border-box';
 body.style.display = 'flex';
 body.style.justifyContent = 'center';
 body.style.alignItems = 'center';
@@ -44,14 +52,13 @@ body.style.alignItems = 'center';
 
 const root = document.getElementById('root');
 
-root.style.width = '50vw';
+root.style.width = '100%';
 root.style.height = '100vh';
 root.style.backgroundColor = 'cadetblue'
 
 root.style.display = 'flex';
 root.style.justifyContent = 'center';
 root.style.alignItems = 'center';
-
 
 console.log(root);
 
@@ -62,19 +69,61 @@ console.log(ul);
 
 root.appendChild(ul);
 
-const li = document.createElement('li');
+ul.style.margin = '0';
+ul.style.padding = '0';
+ul.style.boxSizing = 'border-box';
+ul.style.listStyle = 'none';
 
-console.log(li);
+// gogodanArr 배열의 길이와 같은 개수의 li 태그를 생성하고 스타일 제어하는 반복문 작성
+for(let i = 0; i < gogodanArr.length; i++) {
+  let element = document.createElement('li')
+  element[i] = gogodanArr[i]
+  element.style.padding = '20px';
+  element.style.fontSize = '3vw';
+  element.innerHTML = gogodanArr[i]
+  ul.appendChild(element)
+} 
 
-ul.appendChild(li);
+/* for문을 작성하는 것에 뭔가 착각을 한 듯 이상하게 작성했어서 해멨다.. 이번 기회로 다음에는 헤매는 경우가 적어지거나
+  헤매는 시간이 줄어들것으로 예상한다.
+*/
 
-li.style.listStyle = 'none';
+// function liList() {
+// for(let i = 0; i < gogodanArr.length; i++) {
+//   let element = document.createElement('li')
+//   element[i] = gogodanArr[i]
+//   ul.appendChild(element)
+// }
+//   return ul.li
+// }
+
+// liList()
 
 
-function liList() {
-  for(let i = 1; i < gogodanArr; i ++) {
-    let element = document.createElement('li')
-    li.innerHTML = gogodanArr[i];
-  }
-}
+// for(let i = 0; i < gogodanArr.length; i++) {
+//   let element = document.createElement('li')
+//   element[i] = gogodanArr[i]
+//   ul.appendChild(element)
+// }
+
+// const li = document.querySelector('li')
+
+// console.dir(li)
+
+// const li = document.createElement('li')
+
+// ul.appendChild(li)
+
+
+
+// function liInnerHTML () {
+// for(let i = 0; i < li.length; i++) {
+//   li[i].innerHTML = gogodanArr[i]
+// }
+//   return gogodanArr
+// }
+
+// liInnerHTML()
+
+// console.log(liInnerHTML())
 
